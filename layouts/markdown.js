041bc2a6a73406354c6943f7f29f29ed0a178e6c
Brian Ketelsen <bketelsen@gmail.com>
2021-02-27 03:39:07 -0500

    lite cleanup

--- layouts/markdown.js
+++ layouts/markdown.js
@@ -10,18 +10,16 @@ const DefaultLayout = (props) => {
                 <meta charSet="utf-8" />
             </Head>
             <div>
-                <Navigation/>
-            <header className="bg-white shadow-sm">
+                <Navigation />
+                <header className="bg-white shadow-sm">
                     <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
                         <h1 className="text-lg leading-6 font-semibold text-gray-900">
                             Brian Ketelsen - Notes from the Edge
-                    </h1>
+                        </h1>
                     </div>
                 </header>
                 <main>
-
-                        {props.children}
-  
+                    {props.children}
                 </main>
                 <Footer />
             </div>
